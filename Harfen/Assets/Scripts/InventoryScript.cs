b69703f92450ad8539560ac54f0eb0cf475eb204
Eric Partridge <partre@rpi.edu>
2020-03-03 14:50:16 -0500

    Added some UI elements
    (press 1 for sword, 2 for magic, q to clear hand)

--- Harfen/Assets/Scripts/InventoryScript.cs
+++ Harfen/Assets/Scripts/InventoryScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour
 {
@@ -9,33 +10,45 @@ public class InventoryScript : MonoBehaviour
     public Vector3 swordRotation;
     public GameObject sword;
     public GameObject hand;
+    public Texture emptyUIImage;
+    public Texture swordUIImage;
+    public Texture magicUIImage;
+    public RawImage currentUIImage;
 
     private bool swordEquipped = false;
     private GameObject swordGO;
+    private bool magicEquipped = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentUIImage.texture = emptyUIImage;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //remove sword (or current item in future)
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Destroy(swordGO);
+            swordEquipped = false;
+            magicEquipped = false;
+            currentUIImage.texture = emptyUIImage;
+        }
         //equip sword
-        if (Input.GetKeyDown(KeyCode.Alpha1) && !swordEquipped)
+        else if (Input.GetKeyDown(KeyCode.Alpha1) && !swordEquipped)
         {
             swordGO = Instantiate(sword) as GameObject;
             swordGO.transform.SetParent(hand.transform);
             swordGO.transform.localPosition = swordPosition;
             swordGO.transform.localEulerAngles = swordRotation;
             swordEquipped = true;
+            currentUIImage.texture = swordUIImage;
         }
-        //remove sword (or current item in future)
-        if (Input.GetKeyDown(KeyCode.Q))
+        else if(Input.GetKeyDown(KeyCode.Alpha2) && !magicEquipped)
         {
-            Destroy(swordGO);
-            swordEquipped = false;
+            currentUIImage.texture = magicUIImage;
         }
     }
 
