21a6b5f4f41077d1489a44f798980c1c8d2d0588
Eric Partridge <partre@rpi.edu>
2020-03-03 14:59:57 -0500

    Forgot to add UI for sword and magic combined (press 3)

--- Harfen/Assets/Scripts/InventoryScript.cs
+++ Harfen/Assets/Scripts/InventoryScript.cs
@@ -13,11 +13,13 @@ public class InventoryScript : MonoBehaviour
     public Texture emptyUIImage;
     public Texture swordUIImage;
     public Texture magicUIImage;
+    public Texture magicAndSwordUIImage;
     public RawImage currentUIImage;
 
     private bool swordEquipped = false;
     private GameObject swordGO;
     private bool magicEquipped = false;
+    private bool swordMagicEqupped = false;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +36,7 @@ public class InventoryScript : MonoBehaviour
             Destroy(swordGO);
             swordEquipped = false;
             magicEquipped = false;
+            swordMagicEqupped = false;
             currentUIImage.texture = emptyUIImage;
         }
         //equip sword
@@ -48,7 +51,23 @@ public class InventoryScript : MonoBehaviour
         }
         else if(Input.GetKeyDown(KeyCode.Alpha2) && !magicEquipped)
         {
+            magicEquipped = true;
             currentUIImage.texture = magicUIImage;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3) && !swordMagicEqupped)
+        {
+            if(!swordEquipped)
+            {
+                swordGO = Instantiate(sword) as GameObject;
+                swordGO.transform.SetParent(hand.transform);
+                swordGO.transform.localPosition = swordPosition;
+                swordGO.transform.localEulerAngles = swordRotation;
+            }
+            swordEquipped = true;
+            magicEquipped = true;
+            swordMagicEqupped = true;
+            currentUIImage.texture = magicAndSwordUIImage;
+
         }
     }
 
